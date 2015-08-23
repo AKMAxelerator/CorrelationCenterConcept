c9b6d3b8830e48a436e72f865fcea8ca5313ccae
konard <drakonard@gmail.com>
2015-08-23 13:48:45 +0000

    Editor bug fixes.

---
+++ editor.js
@@ -0,0 +1,73 @@
+(function() {
+
+var editor;
+var editorJSONToSet = "";
+
+// TODO: Узнать почему нельзя сделать редактор с развёрнутым деревом по умолчанию. Возможно нужно написать автору.
+
+var options = {
+  mode: 'tree',
+  modes: ['tree', 'text', 'code'], // allowed modes ('code' не работает, разобраться, почему)
+  error: function (error) { console.dir(error); },
+  change: function () {
+    try {
+      var text = editor.getText();
+      var isTextValid = true;
+      try {
+        if(editor.editor) {
+          if(JSON.stringify(JSON.parse(text)) == editorJSONToSet)
+            return;
+        } else{
+          JSON.parse(text);
+        }
+      } catch (error) { isTextValid = false; }
+      
+      if (isTextValid)
+      {
+        json = editor.get();
+        json = fixChain(json); // TODO: Попробовать возвращать true в том случае, если реально были применены исправления
+        
+        var lastCursorPosition;
+        if(editor.editor)
+        {
+          lastCursorPosition = editor.editor.getCursorPosition();
+          editorJSONToSet = JSON.stringify(json);
+        }
+        
+        editor.set(json);
+        
+        if(editor.editor)
+          editor.editor.moveCursorTo(lastCursorPosition.row, lastCursorPosition.column);
+        
+        copyDefaultChainToStorage(json);
+        if(editor.expandAll) editor.expandAll();
+      }
+      
+      console.dir();
+
+    } catch(error) {
+      console.dir(error);
+    }
+  }
+};
+
+//var leftContainer = document.getElementById('left-json-editor-container');
+//var rightContainer = document.getElementById('right-json-editor-container');
+var container = $('#json-editor-container');
+
+container.css("background-color", "#FFFFFF");
+container.css("width", getSectionWidth());
+container.css("height", getSectionHeight());
+
+var json = copyDefaultChainFromStorage();
+
+//var leftEditor = new JSONEditor(leftContainer, options, json);
+//var rightEditor = new JSONEditor(rightContainer, options, json);
+editor = new JSONEditor(container[0], options, json);
+editor.expandAll();
+
+// TODO: Решить проблему с конфликтами названий css классов json editor и bootstrap.
+
+//window.parent.location.reload();
+
+})();
--- storage.js
+++ storage.js
@@ -48,15 +48,73 @@ var defaultChainItemName = "0";
 function copyDefaultChainFromStorage()
 {
     var chain = JSON.parse(localStorage.getItem(defaultChainItemName));
-    
-    // TODO: Подумать, есть ли более простой способ сделать поле необязательным для редактора
-    for (var i = chain.links.length - 1; i >= 0; i--)
-        if(chain.links[i].value == undefined) chain.links[i].value = 10;
-        
-    return chain;
+    return fixChain(chain);
 }
 
-function copyDefaultChainToChain(value)
+function copyDefaultChainToStorage(value)
 {
     localStorage.setItem(defaultChainItemName, JSON.stringify(value));
+}
+
+function fixChain(chain)
+{
+  if(_.isObject(chain))
+  {
+    if (_.isArray(chain.nodes))
+    {
+      chain.nodes = _.filter(chain.nodes, function(node) { return _.isObject(node); });
+      
+      var max = _.max(chain.nodes, function(node) { return _.isNumber(node.id) && !isNaN(node.id) ? node.id : 0; });
+      var maxId = max.id;
+      
+      if(!maxId || maxId < 0)
+        maxId = 0;
+      
+      for (var i = chain.nodes.length - 1; i >= 0; i--) {
+        var node = chain.nodes[i];
+        
+        if(!_.isObject(node)) continue;
+        
+        // Устанавливаем значения "по умолчанию" или "заглушки" на случай отсутствия их в структуре данных для каждого элемента
+        if(!_.isNumber(node.id) && !_.isString(node.id)) node.id = ++maxId;
+        if(!_.isNumber(node.parent) && !_.isString(node.parent) && node.parent !== null) node.parent = null;
+        if(!_.isString(node.name)) node.name = "" + node.id;
+        if(!_.isString(node.type)) node.type = node.name;
+      }
+    }
+    else
+    {
+      chain.nodes = new Array();
+    }
+    
+    if (_.isArray(chain.links)) // Если у нас есть свойство/аттрибут links + проверим сразу, является ли links массивом/последовательностью.
+    {
+      // TODO: Подумать, есть ли более простой способ сделать поле необязательным для редактора - 
+      // ответ на этот вопрос такой - нужно чтобы перед тем как приступать к дальнейшим изменениям редактор запросил текущее состояние объекта,
+      // но чтобы его запросить нужно будет выполнить этот код, а значит к объекту применятся исправления (также такое поведение будет полезно тогда,
+      // когда сама визуализация начнёт пополняться функциями редактирования)
+      
+      var firstNode = _.find(chain.nodes, function(node){ return _.isObject(node); });
+      
+      for (var i = chain.links.length - 1; i >= 0; i--)
+      {
+        var link = chain.links[i];
+        
+        if(!_.isObject(link)) continue;
+        
+        // Устанавливаем значения "по умолчанию" или "заглушки" на случай отсутствия их в структуре данных для каждой связи
+        if(!_.isNumber(link.source)) link.source = firstNode ? firstNode.id : 0;
+        if(!_.isNumber(link.target)) link.target = firstNode ? firstNode.id : 0;
+        if(!_.isNumber(link.value)) link.value = 1;
+      }
+    }
+    else // Иначе. Т.е. если у нас нет свойства/аттрибута links
+    {
+      chain.links = new Array(); // Надеюсь, что пустого массива будет достаточно, это мы чуточку позже проверим.
+    }
+    
+    //return JSON.parse(JSON.stringify(chain)); // Для защиты от повреждений данных, желательно сделать копию.
+  }
+  
+  return chain;
 }
